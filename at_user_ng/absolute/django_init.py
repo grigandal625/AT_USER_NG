b9f54a626892b175c5dca9764017dfb6f15593c1
grigandal625 <grigandal625@gmail.com>
2026-07-20 19:53:29 +0300

    fix getting module

--- at_user_ng/absolute/django_init.py
+++ at_user_ng/absolute/django_init.py
@@ -6,12 +6,12 @@ from django.core.asgi import get_asgi_application
 from at_user_ng.core.arguments import get_args
 from at_user_ng.utils.settings import get_django_settings_module
 
+args = get_args()
+
 settings_module = get_django_settings_module()
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 django.setup()
-
-args = get_args()
 
 django_application = get_asgi_application()
 
